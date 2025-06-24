a08ef165900a4b17db82a3a7939dfd441a7af06a
Steven Crawford <steven@shelbybark.com>
2025-06-24 13:50:34 -0500

    updaes

--- lua/shelbybark/plugins/formatting.lua
+++ lua/shelbybark/plugins/formatting.lua
@@ -12,7 +12,7 @@ return {
 				typescriptreact = { "prettier" },
 				svelte = { "prettier" },
 				css = { "prettier" },
-				html = { "prettier", "djlint" },
+				html = { "prettier", "djlint", "htmlbeautifier" },
 				json = { "prettier" },
 				yaml = { "prettier" },
 				markdown = { "prettier" },
